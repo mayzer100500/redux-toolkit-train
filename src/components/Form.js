67ead97d3acf2e4c7e3891e9de86a575acc72537
mayzer100500 <vladimir.gizhitskiy@gmail.com>
2023-06-15 13:58:05 +0300

    add alert on empty todo

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addTodo } from '../features/todo/todoSlice'
 import { v4 as uuidv4 } from 'uuid'
+import Alert from '@mui/material/Alert';
 
 const Form = () => {
     const dispatch = useDispatch()
     const [todoValue, setTodoValue] = useState("")
+    const [alert, setAlert] = useState(false)
 
     const addTodoHandler = () => {
         const todo = {
@@ -13,29 +15,37 @@ const Form = () => {
             text: todoValue,
             isCompleted: false,
         }
-        dispatch(addTodo(todo))
-        setTodoValue("")
+        if (todo.text === "") {
+            setAlert(true)
+        } else {
+            dispatch(addTodo(todo))
+            setTodoValue("")
+            setAlert(false)
+        }
     }
 
-
     return (
-        <form className='w-full flex' onSubmit={(e) => e.preventDefault()}>
-            <input
-                type='text'
-                name="todoItem"
-                value={todoValue}
-                placeholder='Type something...'
-                className='w-full p-1 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
-                onChange={(e) => setTodoValue(e.target.value)}
-            />
-            <button
-                type='submit'
-                className='shrink-0 bg-lime-300  hover:bg-lime-400 transition-all px-3 text-sm'
-                onClick={() => addTodoHandler()}
-            >
-                Submit
-            </button>
-        </form>
+        <div>
+            <form className='w-full flex' onSubmit={(e) => e.preventDefault()}>
+                <input
+                    type='text'
+                    name="todoItem"
+                    value={todoValue}
+                    placeholder='Type something...'
+                    className='w-full p-1 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
+                    onChange={(e) => setTodoValue(e.target.value)}
+                />
+                <button
+                    type='submit'
+                    className='shrink-0 bg-lime-300  hover:bg-lime-400 transition-all px-3 text-sm'
+                    onClick={() => addTodoHandler()}
+                >
+                    Submit
+                </button>
+            </form>
+            {alert &&
+                <Alert variant="filled" severity="warning">Please, fill todo!!!</Alert>}
+        </div>
     )
 }
 
